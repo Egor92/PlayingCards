65d82948fa1b0021c9cbbe61df54e820ef430ded
Egor Novikov <novikov.ea@mail.ru>
2016-09-24 12:20:35 +0300

    IEquitable implemented #1

--- code/Egor92.PlayingCards/Card.cs
+++ code/Egor92.PlayingCards/Card.cs
@@ -1,6 +1,8 @@
-﻿namespace Egor92.PlayingCards
+﻿using System;
+
+namespace Egor92.PlayingCards
 {
-    public class Card : ICard
+    public class Card : ICard, IEquatable<Card>
     {
         #region Ctor
 
@@ -16,13 +18,13 @@
 
         #region Rank
 
-        public Rank Rank { get; private set; }
+        public Rank Rank { get; }
 
         #endregion
 
         #region Suit
 
-        public Suit Suit { get; private set; }
+        public Suit Suit { get; }
 
         #endregion
 
@@ -36,5 +38,51 @@
         #endregion
 
         #endregion
+
+        #region Overridden members
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return Equals((Card)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((int)Rank * 397) ^ (int)Suit;
+            }
+        }
+
+        #endregion
+
+        #region Implementation of IEquatable<Card>
+
+        public bool Equals(Card other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return Rank == other.Rank && Suit == other.Suit;
+        }
+
+        #endregion
+
+        public static bool operator ==(Card left, Card right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Card left, Card right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
