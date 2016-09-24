4ca9ad5b6a3205b4355b938795ed0096cfaa30bd
Egor Novikov <novikov.ea@mail.ru>
2016-09-24 15:25:28 +0300

    AssemblyInfo changed

--- code/Egor92.PlayingCards/Properties/AssemblyInfo.cs
+++ code/Egor92.PlayingCards/Properties/AssemblyInfo.cs
@@ -1,18 +1,16 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Egor92.PlayingCards")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple playing cards implementation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Novikov Egor")]
 [assembly: AssemblyProduct("Egor92.PlayingCards")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Novikov Egor 2016")]
+[assembly: AssemblyTrademark("Novikov Egor")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
